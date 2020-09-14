85fd42828bb79325b064d513d145117d503a4393
czerwo <czerwonykoczkodan@gmail.com>
2020-09-15 00:45:58 +0200

    Add project files.

---
+++ client.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+#include <WS2tcpip.h>
+#include <string>
+
+#pragma commment(lib,"ws2_32.lib");
+
+int main()
+{
+	//initialize winsock
+
+	WSADATA myData;
+
+	WORD ver = MAKEWORD(2, 2);
+
+	int wsOk = WSAStartup(ver, &myData);
+	if (wsOk != 0)
+	{
+		std::cerr << "Can't initialize winsock. Quitting" << std::endl;
+		return 0;
+	}
+
+	//create a socket
+
+	SOCKET listening = socket(AF_INET, SOCK_STREAM, 0);
+
+	if (listening == INVALID_SOCKET)
+	{
+		std::cerr << "Can't create a socket. Quittiing" << std::endl;
+		return 0;
+	}
+
+	//bind the ip address and port to a socket
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(54000);
+	hint.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	bind(listening, (sockaddr*)&hint, sizeof(hint));
+
+
+	//tell winsock the socket is for listening
+
+	listen(listening, SOMAXCONN);
+
+	//wait for a connection
+
+	sockaddr_in client;
+	int clientSize = sizeof(client);
+
+	SOCKET clientSocket = accept(listening, (sockaddr*)&client, &clientSize);
+	if (clientSocket == INVALID_SOCKET)
+	{
+		std::cerr << "Something wrong with waiting for connection. Quitting" << std::endl;
+		return 0;
+	}
+
+	char host[NI_MAXHOST];		//Client's remote name
+	char service[NI_MAXSERV];	//Service (i.e. port) the client is connect on
+
+	ZeroMemory(host, NI_MAXHOST);
+	ZeroMemory(service, NI_MAXSERV);
+	if (getnameinfo((sockaddr*)&client, sizeof(client), host, NI_MAXHOST, service, NI_MAXSERV, 0) == 0)
+	{
+		std::cout << host << " connected on port: " << service << std::endl;
+	}
+	else
+	{
+		inet_ntop(AF_INET, &client.sin_addr, host, NI_MAXHOST);
+		std::cout << host << " connected on port: " << ntohs(client.sin_port) << std::endl;
+	}
+
+	//close listening socket
+
+	closesocket(listening);
+
+	//while loop: accept and echo message back to client
+
+	char buf[4096];
+
+	while (true)
+	{
+		ZeroMemory(buf, 4096);
+
+		//wait for client to send data
+		int bytesReceived = recv(clientSocket, buf, 4096, 0);
+
+		if (bytesReceived == SOCKET_ERROR)
+		{
+			std::cerr << "Error in recv(). Quitting" << std::endl;
+			break;
+		}
+
+		if (bytesReceived == 0)
+		{
+			std::cout << "Client disconnected" << std::endl;
+			break;
+		}
+
+		//echo message back to client
+		send(clientSocket, buf, bytesReceived + 1, 0);
+
+
+	}
+
+	//close the sock
+	closesocket(clientSocket);
+
+	//shutdown winsock
+	WSACleanup();
+}
